64d6ef42c8c2cc8a2cbd2834576f46c3cb24b6c9
rony31416 <ronymajumder1171@gmail.com>
2023-01-22 00:19:27 +0600

    modified

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,64 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
 using namespace std;
+
+void openFile(ifstream&, string);
+void processFile_wordByword(ifstream &);
+map<string,int> wordCount;
+
+
 int main()
 {
-   cout<<"jjjj\n";
+    ifstream FILE;
+    openFile(FILE,"input.txt");
+    processFile_wordByword(FILE);
+    //int i = 1;
+    for(auto u : wordCount)
+    {
+        cout<<u.first<<" "<<u.second<<"\n";
+    }
 
+    FILE.close();
     return 0;
+}
+
+void openFile(ifstream &FILE, string fileName)
+{
+    FILE.open(fileName);
+    if(!FILE.is_open())
+    {
+         cout<<"Failed to open file\n";
+    }
+
+}
+void processFile_wordByword(ifstream &FILE)
+{
+    string line,word;
+    istringstream iss;
+    fstream f;
+    f.open("write.txt");
+    while(!FILE.eof())
+    {
+        getline(FILE,line);
+        if(FILE.good())
+        {
+            iss.clear();
+            iss.str(line);
+            while(iss.good())
+            {
+                iss>>word;
+                cout<<word<<" ";
+                wordCount[word]++;
+            }
+            cout<<"\n";
+        }
+    }
+
+    for(auto str : wordCount)
+    {
+        f<<str.first<<" "<<str.second<<"\n";
+    }
+
 }
