9c0a3ddd13e7a742a9b2527e4604978b72081afb
rony31416 <ronymajumder1171@gmail.com>
2023-02-06 02:38:26 +0600

    modified

--- Main.cpp
+++ Main.cpp
@@ -1,96 +1,27 @@
 #include <iostream>
+#include <string>
 #include <fstream>
+#include <algorithm>
 #include <sstream>
-#include <map>
+#include "stringLibrary.h"
 using namespace std;
-
-void openFile(ifstream&, string);
-void processFile_wordByword(ifstream &);
-map<string,int> wordCount;
-map<string,int> storeWord,stopWord;
-map<string,int> wordAfterRemoveSw;
+#define N 1000
 
 
-int main()
+string words[N];
+string Bochon_suffix[N];
+string Bivokti_suffix[N];
+string quantifier[N];
+string stopWord[N];
+string tags[N];
+string tokenizedWords[N];
+
+
+void print(string s[])
 {
-    wordCount.clear();
-    storeWord.clear();
-    stopWord.clear();
-    wordAfterRemoveSw.clear();
+    for(int i = 0 ; i < countElements(s) ; i++) cout<<s[i]<<"\n";
 
-    ifstream FILE;
-    openFile(FILE,"input.txt");
-    processFile_wordByword(FILE);
-    FILE.close();
-
-
-
-    openFile(FILE,"write.txt");
-    string str,line,word;
-    istringstream iss;
-    while(!FILE.eof())
-    {
-        getline(FILE,line);
-        if(FILE.good())
-        {
-            iss.clear();
-            iss.str(line);
-            while(iss.good())
-            {
-                iss>>word;
-                storeWord[word]++;
-            }
-        }
-    }
-
-    FILE.close();
-
-    openFile(FILE,"stopWords.txt");
-
-
-    while(!FILE.eof())
-    {
-        getline(FILE,line);
-        if(FILE.good())
-        {
-            iss.clear();
-            iss.str(line);
-            while(iss.good())
-            {
-                iss>>word;
-                stopWord[word]++;
-            }
-        }
-    }
-    cout<<storeWord.size()<<"\n";
-    for(auto u : storeWord) cout<<u.first<<"\n";
-
-    FILE.close();
-
-    fstream f;
-    f.open("swRemove.txt");
-
-    std::map<std::string, int>::iterator it = storeWord.begin();
-
-    for(int i = 0 ; i < storeWord.size(); i++,it++)
-    {
-        string word = it->first;
-        bool ok = false;
-
-        //cout<<"word : "<<word<<"\n";
-        for(auto  u : stopWord)
-        {
-            if(u.first == word)
-            {
-                ok = true;
-                cout<<"paisi :: "<<word<<"\n";
-            }
-        }
-
-        if(!ok) f<<word<<"\n";
-
-    }
-    return 0;
+    cout<<"------------------------------\n";
 }
 
 void openFile(ifstream &FILE, string fileName)
@@ -102,12 +33,12 @@ void openFile(ifstream &FILE, string fileName)
     }
 
 }
+
 void processFile_wordByword(ifstream &FILE)
 {
     string line,word;
     istringstream iss;
-    fstream f;
-    f.open("write.txt");
+    int i = 0;
     while(!FILE.eof())
     {
         getline(FILE,line);
@@ -118,16 +49,124 @@ void processFile_wordByword(ifstream &FILE)
             while(iss.good())
             {
                 iss>>word;
-                //  cout<<word<<" ";
-                wordCount[word]++;
+                words[i] = word;
+                i++;
             }
-            // cout<<"\n";
         }
     }
+}
 
-    for(auto str : wordCount)
+void storeText()
+{
+    ifstream FILE;
+    openFile(FILE,"input.txt");
+    processFile_wordByword(FILE);
+
+    FILE.close();
+}
+
+void storeSuffixFromTextFile(string name)
+{
+    ifstream FILE;
+    string s;
+    int i = 0;
+
+    if(name == "bochon")
     {
-        f<<str.first<<"\n";//<<str.second<<"\n";
+        openFile(FILE,"BochonSuffix.txt");
+        while(getline(FILE,s))
+        {
+            Bochon_suffix[i] = s;
+            i++;
+        }
+        FILE.close();
     }
+    else if(name == "bivokti")
+    {
+        openFile(FILE,"BivoktiSuffix.txt");
+        while(getline(FILE,s))
+        {
+            Bivokti_suffix[i] = s;
+            i++;
+        }
+        FILE.close();
+    }
+    else if(name == "quantifier")
+    {
+        openFile(FILE,"Quantifiers.txt");
+        while(getline(FILE,s))
+        {
+            quantifier[i] = s;
+            i++;
+        }
+        FILE.close();
+    }
+    else if(name == "stopword")
+    {
+        openFile(FILE,"stopWords.txt");
+        while(getline(FILE,s))
+        {
+            stopWord[i] = s;
+            i++;
+        }
+        FILE.close();
+    }
+}
 
+void storeSuffix()
+{
+    storeSuffixFromTextFile("bochon");
+    storeSuffixFromTextFile("bivokti");
+    storeSuffixFromTextFile("quantifier");
+    storeSuffixFromTextFile("stopword");
+}
+
+void Tokenization()
+{
+    int k = 0;
+
+    int n = countElements(words);
+    for (int i = 0; i < n; i++)
+    {
+        char* updatedStr = new char[N];
+        string DARI1 = "।";
+        string DARI2 = "৷";
+
+        int find1 = stringPatternSearch(words[i],DARI1);
+        int find2 = stringPatternSearch(words[i],DARI2);
+
+        if(find1 != -1 || find2 != -1)
+        {
+            if(find1 != -1)
+                words[i] = replaceFirstOccurrence(words[i],DARI1,"#");
+            else
+                words[i] = replaceFirstOccurrence(words[i],DARI2,"#");
+        }
+
+        int idx = 0;
+        for (int j = 0; j < words[i].size(); j++)
+        {
+            if (!ispunct(words[i][j]) && words[i][j]!='#')
+                updatedStr[idx++] = words[i][j];
+        }
+
+        for(int j = 0; j < idx; j++)
+            tokenizedWords[k].push_back(updatedStr[j]);
+
+        k++;
+    }
+}
+
+int main()
+{
+    storeText();
+    storeSuffix();
+
+    Tokenization();
+
+    print(words);
+    print(tokenizedWords);
+
+
+    return 0;
 }
--- stringLibrary.h
+++ stringLibrary.h
@@ -1,10 +1,11 @@
-#ifndef STRINGLIBRARY_H
-#define STRINGLIBRARY_H
+#ifndef STRINGOPERATIONS_H
+#define STRINGOPERATIONS_H
 #include<iostream>
 #include<algorithm>
+
 using namespace std;
 
-int strlen(string str)
+int len(string str)
 {
     int length = 0;
     for (int i = 0; str[i] != '\0'; i++)
@@ -14,7 +15,140 @@ int strlen(string str)
     return length;
 }
 
+int countElements(string str[])
+{
+    int count = 0;
+    int i = 0;
+    while (!str[i].empty())
+    {
+        count++;
+        i++;
+    }
+    return count;
+}
+//---------------------------------------------------------------
+//Rabin-Karp Algorithm for Pattern Searching
+int stringPatternSearch(string text,string pattern)
+{
+
+    const int BASE = 256;
+    const int MOD = 1e9 + 7;
+
+    // pattern hash koto?
+    long long pattern_hash = 0;
+    for (int i = 0; i < pattern.size(); ++i)
+    {
+        int new_char = pattern[i] - 'a' + 1;
+        pattern_hash = (pattern_hash * BASE + new_char) % MOD;
+    }
+
+    long long text_window_hash = 0;
+    for (int i = 0; i < pattern.size(); ++i)
+    {
+        int new_char = text[i] - 'a' + 1;
+        text_window_hash = (text_window_hash * BASE + new_char) % MOD;
+    }
+
+    // power = BASE^(|pattern|-1)
+    long long power = 1;
+    for (int i = 0; i < (int) pattern.size() - 1; ++i)
+    {
+        power = power * BASE % MOD;
+    }
+
+    // text er surur 3 size window ase
+    for (int i = 0, j = (int) pattern.size() - 1; j < text.size(); ++i, ++j)
+    {
+        if (text_window_hash == pattern_hash)
+        {
+            return i;
+        }
+
+        // leftmost char ta baad dite hobe
+        int out_char = text[i] - 'a' + 1;
+        // here power = BASE^(|pattern|-1)
+        text_window_hash = (text_window_hash - out_char * power) % MOD;
+        if (text_window_hash < 0) text_window_hash += MOD;
+
+        text_window_hash = text_window_hash * BASE % MOD;
+
+        if (j + 1 < text.size())
+        {
+            int in_char = text[j + 1] - 'a' + 1;
+            text_window_hash = (text_window_hash + in_char) % MOD;
+        }
+    }
+
+    return -1;
+}
+//-----------------------------------------------------------
+//KMP Algorithm for Pattern Searching
+int* computeLPS(string pattern)
+{
+    int patternLength = pattern.size();
+    int* lps = new int[patternLength];
+    lps[0] = 0;
+    int k = 0;
+    for (int i = 1; i < patternLength; i++)
+    {
+        while (k > 0 && pattern[k] != pattern[i])
+        {
+            k = lps[k-1];
+        }
+        if (pattern[k] == pattern[i])
+        {
+            k++;
+        }
+        lps[i] = k;
+    }
+    return lps;
+}
+
+int stringFind(string text, string pattern)
+{
+    int position = -1;
+    int* lps = computeLPS(pattern);
+    int matchedIndex = 0;
+    for (int currentIndex = 0; currentIndex < text.length(); currentIndex++)
+    {
+        while (matchedIndex > 0 && pattern[matchedIndex] != text[currentIndex])
+        {
+            matchedIndex = lps[matchedIndex - 1];
+        }
+        if (pattern[matchedIndex] == text[currentIndex])
+        {
+            matchedIndex++;
+        }
+        if (matchedIndex == len(pattern) )
+        {
+            position = currentIndex - (len(pattern) - 1);
+            matchedIndex = lps[matchedIndex - 1];
+        }
+    }
+    return position;
+}
+//---------------------------------------------------------------
+
+string replaceFirstOccurrence(string inputString, string target, string replacement)
+{
+    string result = "";
+    int targetIndex = stringFind(inputString, target);
+    for(int i = 0; i < targetIndex; i++)
+        result += inputString[i];
+
+    int remainingChars = 1 + inputString.length() - result.length() - target.length();
+
+    for(int i = 0; i < replacement.length(); i++)
+        result += replacement[i];
+
+    for(int i = inputString.length() - remainingChars; i < inputString.length(); i++)
+        result += inputString[i];
+
+    return result;
+}
+
 
 
 
 #endif
+
